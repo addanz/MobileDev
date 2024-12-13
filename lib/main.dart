0635d03d11031ff0c59fc4a0d03ff36c75889549
addan <addanzahra@gmail.com>
2024-12-13 18:15:30 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+import 'screens/aboutme.dart';
+import 'screens/contact.dart';
+import 'widget/portfoliotitle.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Addan\'s Portfolio',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Addan\'s Portfolio'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              child: const Text(
+                'Navigation',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('About Me'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const AboutMeScreen()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.contact_mail),
+              title: const Text('Contact'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ContactScreen()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Welcome Section
+              Card(
+                elevation: 4,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Welcome to My Portfolio!',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      const Text(
+                        'Hi, I\'m Addan, a passionate Computer Programming student. This website is for my Mobile Development course by '
+                        'the code of INFT 3101-01. This website is meant to allow you to explore fun things about me and the work I have '
+                        'completed throughout my computer programming career so far. Explore my projects, labs, and work! Please use '
+                        'the navigation bar to access the different pages!',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              // Portfolio Section
+              Card(
+                elevation: 4,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Portfolio',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      PortfolioTile(
+                        title: 'Lab Work',
+                        description: 'In labs, I got to practice and experiment with Android Studio, MIT App Inventor, and DartPad. Using these different apps, '
+                            'I made multiple activities with multiple screens, sharing things about myself like my favourite quote, making quizzes to try out and quiz my friends. '
+                            'I also created informative blog pages about what Arbaeen is and my experience with it. On this lab about Arbaeen, I incorporated a checklist to help others '
+                            'plan for their trip as well.',
+                        icon: Icons.laptop,
+                      ),
+                      PortfolioTile(
+                        title: 'Classwork',
+                        description: 'For classwork, I did things similar to those mentioned for the lab work but this time experimented with different types of coding, technologies, frameworks, '
+                            'and tools. Some of the different things we tried out included Java, Flutter, Dart, HTML/CSS, JavaScript, Bootstrap, and WordPress, '
+                            'each applied to different aspects of programming and development.',
+                        icon: Icons.notes,
+                      ),
+                      PortfolioTile(
+                        title: 'Projects',
+                        description: 'For projects, this is my biggest individual project aside from the group project where we built a game called Maze Maureders. In this game, you pick a character '
+                            'and start in a maze. As you open doors, you unlock more of the maze map and face attackers. You can either use a defensive shielding ability or an attacking ability '
+                            'to defeat the attackers. It is a timed game, so whoever defeats all attackers the fastest gets their record on the leaderboard. I will attach images to display my work:',
+                        icon: Icons.folder,
+                      ),
+                      const SizedBox(height: 16),
+                      // Add images side by side
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Image.asset(
+                                'assets/Picture1.png', 
+                                fit: BoxFit.contain,
+                                height: 500,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Image.asset(
+                                'assets/Picture2.png', 
+                                fit: BoxFit.contain,
+                                height: 500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Image.asset(
+                                'assets/Picture3.png', 
+                                fit: BoxFit.contain,
+                                height: 500,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Image.asset(
+                                'assets/Picture4.png', 
+                                fit: BoxFit.contain,
+                                height: 500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/aboutme.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class AboutMeScreen extends StatelessWidget {
+  const AboutMeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('About Me'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Addan Zahra',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Center(
+              child: CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage('assets/me.jpg'),
+              ),
+            ),
+            const SizedBox(height: 24),
+            const Divider(),
+            const SizedBox(height: 16),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Icon(Icons.flag, color: Colors.green),
+                const SizedBox(width: 14),
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Background:',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'I am a Pakistani-Canadian. Born and raised in Canada but am strongly connected with my culture and background. Despite being born here and living here, '
+                        'I was raised in a way where English was actually my second language and I learned Urdu first. Urdu is the national language of Pakistan.',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            const Divider(),
+            const SizedBox(height: 16),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Icon(FontAwesomeIcons.laptopCode, color: Colors.purple),
+                const SizedBox(width: 14),
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Education:',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'I am a third-year student specializing in web and app development. I am currently in my last year and will graduate in April. I have always been a student that '
+                        'aims to get the highest grades and honour rolls. I currently have a 4.6 GPA out of a 5.0 GPA.',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            const Divider(),
+            const SizedBox(height: 16),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Icon(FontAwesomeIcons.book, color: Colors.blue),
+                const SizedBox(width: 14),
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Hobbies:',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'I like reading books and collecting them as a hobby. I want to create my own library one day and am working towards building it right now by first collecting '
+                        'books. I even ordered myself a stamp that says this belongs to the library of Addan Zahra and I stamp all my books with it to keep the collection together '
+                        'and personalize it. I have always loved to read books since I was little, especially fictional ones. I even started writing my own short story back in grade 8 '
+                        'because of my love for books and novels.',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            const Divider(),
+            const SizedBox(height: 16),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Icon(FontAwesomeIcons.paintBrush, color: Colors.red),
+                const SizedBox(width: 14),
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Free Time:',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'In my free time, I enjoy doing lots of arts and crafts activities. I like to make them both for my friends and family and also to decorate my room.',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            const Divider(),
+            const SizedBox(height: 16),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Icon(FontAwesomeIcons.handshake, color: Colors.orange),
+                const SizedBox(width: 14),
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Interests/ Community Involvement:',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'I love to give back to the community and help others. I am actively involved in community activities, engaging with children, friends, and elders, '
+                        'especially at the mosque. I organize and participate in events that bring people together and provide support for those in need. Giving back to '
+                        'the community gives me a sense of fulfillment and joy.',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
